971bfa623daaa47da92ccdc6595785463c2aa12b
tim-jacobs <jacobstim15@gmail.com>
2025-05-16 13:13:55 -0500

    Initial commit to repo

---
+++ feature.php
@@ -0,0 +1,15 @@
+
+<?php  
+/* echo is a print command */
+echo "This is a PHP program.";  
+
+<div class="feature">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8">CONTENT HERE</div>
+            <div class="col-xs-12 col-sm-4">BUTTON HERE</div>
+        </div>
+    </div>
+</div>
+
+?> 
---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html><!--<![endif]-->
+  <head>
+    <!-- IE 8 Fix -->
+    <title>{$page_title}</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="generator" content="http://www.getLiveEdit.com" />
+    <meta name="Keywords" content="{$meta_tags}" />
+    <meta name="Description" content="{$meta_description}" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+    />
+    <meta name="format-detection" content="telephone=no" />
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <!-- *Ie 8 Bootstrap Fix* -->
+    <!--[if IE 8]>
+      <link
+        rel="stylesheet"
+        href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
+        type="text/css"
+      />
+      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link
+      href="https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400italic,700,700italic"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic,700"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="./styles.css" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+  </head>
+  <html>
+    <body>
+      <div class="siteMain">
+        <div class="infoBar">
+          <div class="container infoBar-inner">
+            <p class="whiteText">
+              <i class="fa fa-map-marker"></i> St. Joseph, MN
+              <span class="text-spacer"></span>
+              <i class="fa fa-phone"></i> (320) 247-3381
+              <span class="text-spacer"></span>
+              <i class="fa fa-envelope"></i>
+              <i class="fa fa-facebook-official" aria-hidden="true"></i>
+              <a href="mailto:Dynamicdetailingmn@gmail.com" target="_blank" class="whiteText">Dynamicdetailingmn@gmail.com</a>
+            </p>
+          </div>
+        </div>
+        <div class="logoMenu">
+          <div class="container">
+            <div class="row">
+              <div class="logo col-sm-12 col-md-3">
+                <a href="/index.html" target="_self" aria-label="Logo link to Home Page">
+                  <img
+                    src="images/dynamicDetailing_logoWeb.png" alt="Dynamic Detailing Logo"
+                  />
+                </a>
+              </div>
+              <div class="menu col-sm-12 col-md-9">
+                <ul>
+                  <li><a href="/home">Home</a></li>
+                  <li><a href="/about-us">About Us</a></li>
+                  <li><a href="/services">Services</a></li>
+                  <li><a href="/gallery">Gallery</a></li>
+                  <li><a href="/contact-us">Contact Us</a></li>
+                </ul>
+                <div class="clear"></div>
+              </div>
+              <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+          </div>
+        </div>
+
+        <header class="header">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="images/mainHeader1.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/mainHeader2.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/mainHeader3.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </header>
+
+        <section class="mainContent">
+          <div class="container">
+            <div class="row">
+              <div class="mainContent-inner">
+                <div class="text-center">
+                  <h4>Central MN's Own</h4>
+                  <h2 class="titleUnderline">Dynamic Detailing</h2>
+                  <p>At Dynamic Detailing we offer premium automotive detailing services that come to you.</p>
+                  <p>Call us TODAY at (320) 247-3381</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="whyChooseUs">
+          <div class="shade">
+            <div class="container">
+              <div class="row">
+                <div class="logo col-sm-12 col-md-6"><img src="images/truck-front.jpg" width="100%" height="auto" /></div>
+                <div class="logo col-sm-12 col-md-6">
+                  <h3 class="whiteText">Why Choose Us</h3>
+                  <p class="p1 whiteText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+              </div>
+
+              <div class="height-spacer"></div>
+
+              <div class="row">
+                <div class="logo col-sm-12 col-md-6">
+                  <h3 class="whiteText">We Get Shit Done</h3>
+                  <p class="p1 whiteText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="logo col-sm-12 col-md-6"><img src="images/truck-inside.jpg" width="100%" height="auto" /></div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="mainServices"></section>
+
+        <?php include("feature.php");?>
+
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="logo col-sm-12 col-md-4"><p class="whiteText">&copy; Dynamic Detailing 2024</p></div>
+              <div class="logo col-sm-12 col-md-4"><p>&nbsp;</p></div>
+              <div class="logo col-sm-12 col-md-4 text-right">
+                <p class="whiteText">
+                  <a href="https://www.facebook.com/profile.php?id=100092265796496" class="whiteText" target="_blank">facebook</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+      
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+  </html>
